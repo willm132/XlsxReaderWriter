e1f82120cf4b3581a6f51b0c7caed565aad87401
Will Mays <will.mays@icloud.com>
2025-12-15 09:58:30 -0500

    Create MinizipPrefix.h

---
+++ Sources/XlsxReaderWriter/minizip/MinizipPrefix.h
@@ -0,0 +1,16 @@
+// MinizipPrefix.h
+#define zipOpen            xr_zipOpen
+#define zipOpen64          xr_zipOpen64
+#define zipOpen2           xr_zipOpen2
+#define zipOpen2_64        xr_zipOpen2_64
+#define zipOpenNewFileInZip        xr_zipOpenNewFileInZip
+#define zipOpenNewFileInZip64      xr_zipOpenNewFileInZip64
+#define zipOpenNewFileInZip5       xr_zipOpenNewFileInZip5
+#define zipWriteInFileInZip        xr_zipWriteInFileInZip
+#define zipCloseFileInZip          xr_zipCloseFileInZip
+#define zipCloseFileInZipRaw       xr_zipCloseFileInZipRaw
+#define zipCloseFileInZipRaw64     xr_zipCloseFileInZipRaw64
+#define zipClose                   xr_zipClose
+
+#define fill_fopen_filefunc        xr_fill_fopen_filefunc
+#define fill_fopen64_filefunc      xr_fill_fopen64_filefunc
